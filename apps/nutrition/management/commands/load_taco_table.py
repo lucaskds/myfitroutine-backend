fa1dace12c7d5c66e978ba030d015439f78a6533
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2022-11-27 13:26:03 -0300

    ✨ Add FoodItem DB preload

---
+++ apps/nutrition/management/commands/load_taco_table.py
@@ -0,0 +1,26 @@
+import csv
+import os
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from apps.nutrition.models import FoodCategory, FoodItem
+
+
+class Command(BaseCommand):
+    help = "Load TACO info to DB."
+
+    def handle(self, *args, **options):
+        file = open(os.path.join(settings.BASE_DIR, "TACO.csv"))
+        read_file = csv.reader(file)
+
+        for record in read_file:
+            category, _ = FoodCategory.objects.get_or_create(label=record[0])
+            FoodItem.objects.get_or_create(
+                category=category,
+                label=record[1],
+                kcal=record[2] or None,
+                protein=record[3] or None,
+                fat=record[4] or None,
+                carbs=record[5] or None,
+                fibers=record[6] or None,
+            )
