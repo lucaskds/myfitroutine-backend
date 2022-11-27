22531cddf13a05be6f4ac64f2462ba97ba42cb8f
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2022-11-27 17:44:54 -0300

    ✨ Add comment and workout duration

--- apps/train/models/diary.py
+++ apps/train/models/diary.py
@@ -6,3 +6,5 @@ class WorkoutDiary(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     date = models.DateField()
     entry = models.JSONField(default=list)
+    duration = models.PositiveIntegerField(default=0)
+    comments = models.TextField(max_length=500, blank=True)
