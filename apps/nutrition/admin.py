d8aba5c5579c5f4845d714737a2081c0fab91193
Lucas Krämer de Sousa <lucas.kramer.sousa@gmail.com>
2022-12-01 23:34:12 -0300

    🔧 Update tables from admin

--- apps/nutrition/admin.py
+++ apps/nutrition/admin.py
@@ -3,5 +3,35 @@ from django.contrib import admin
 from apps.nutrition.models import FoodCategory, FoodItem, NutritionDiary
 
 admin.site.register(FoodCategory)
-admin.site.register(FoodItem)
 admin.site.register(NutritionDiary)
+import csv
+import os
+
+from django.conf import settings
+
+
+@admin.action(description="Update food item table")
+def update_list(modeladmin, request, queryset):
+    file = open(os.path.join(settings.BASE_DIR, "base_data/TACO.csv"))
+    read_file = csv.reader(file)
+
+    for record in read_file:
+        category, _ = FoodCategory.objects.get_or_create(label=record[0])
+        FoodItem.objects.get_or_create(
+            category=category,
+            label=record[1],
+            kcal=record[2] or None,
+            protein=record[3] or None,
+            fat=record[4] or None,
+            carbs=record[5] or None,
+            fibers=record[6] or None,
+        )
+
+
+class FoodItemAdmin(admin.ModelAdmin):
+    list_display = ["category", "label"]
+    ordering = ["label"]
+    actions = [update_list]
+
+
+admin.site.register(FoodItem, FoodItemAdmin)
--- apps/train/admin.py
+++ apps/train/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from apps.train.models import (
     Exercise,
+    ExerciseCategory,
     ExerciseGroup,
+    ExerciseItem,
     ExerciseSet,
     WorkoutDiary,
     WorkoutPlan,
@@ -11,5 +13,35 @@ from apps.train.models import (
 admin.site.register(Exercise)
 admin.site.register(ExerciseGroup)
 admin.site.register(ExerciseSet)
+admin.site.register(ExerciseCategory)
 admin.site.register(WorkoutPlan)
 admin.site.register(WorkoutDiary)
+
+import csv
+import os
+
+from django.conf import settings
+
+from apps.train.models import ExerciseCategory, ExerciseItem
+
+
+@admin.action(description="Update exercises table")
+def update_list(modeladmin, request, queryset):
+    file = open(os.path.join(settings.BASE_DIR, "base_data/exercises.csv"))
+    read_file = csv.reader(file)
+
+    for record in read_file:
+        category, _ = ExerciseCategory.objects.get_or_create(label=record[0])
+        ExerciseItem.objects.get_or_create(
+            category=category,
+            label=record[1],
+        )
+
+
+class ExerciseItemAdmin(admin.ModelAdmin):
+    list_display = ["category", "label"]
+    ordering = ["label"]
+    actions = [update_list]
+
+
+admin.site.register(ExerciseItem, ExerciseItemAdmin)
